069001747d7e17db819e8979338fa6cd49ebc92a
Lash Blast <evelyn.aguirre.9895@gmail.com>
2022-03-07 16:24:09 -0600

    code current date field

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,19 @@
+// the current day is displayed at the top of the calendar
+var todaysDate = document.getElementById("currentDay").innerHTML = "<p>" + moment().format("dddd, MMMM Do, YYYY") + "</p>";
 
 
-
-
+// 
+// GIVEN I am using a daily planner to create a schedule
+// WHEN I open the planner
+// THEN 
+// WHEN I scroll down
+// THEN I am presented with timeblocks for standard business hou
+// WHEN I view the timeblocks for that day
+// THEN each timeblock is color coded to indicate whether it is 
+// present, or future
+// WHEN I click into a timeblock
+// THEN I can enter an event
+// WHEN I click the save button for that timeblock
+// THEN the text for that event is saved in local storage
+// WHEN I refresh the page
+// THEN the saved events persist
