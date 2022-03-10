5d742b95c6e3d4c2830da14dd6ce315db9ccde9f
Lash Blast <evelyn.aguirre.9895@gmail.com>
2022-03-10 11:41:25 -0600

    refine hour blocks and icon position

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,35 @@
 // the current day is displayed at the top of the calendar
 var todaysDateEl = document.getElementById("currentDay").innerHTML = "<p>" + moment().format("dddd, MMMM Do, YYYY") + "</p>";
-// hour of day (NEED TO FIX)
-var timeEl = document.getElementById("hour").innerHTML = "<p>" + moment().format("hA") + "</p>";
 
-
-
-
-
+// style schedule block 
+var eightAMEl= document.getElementById("8-a").innerHTML = "<p>" + moment().format("8A") + "</p>";
+var nineAMEl= document.getElementById("9-a").innerHTML = "<p>" + moment().format("9A") + "</p>";
+var tenAMEl= document.getElementById("10-a").innerHTML = "<p>" + moment().format("10A") + "</p>";
+var elevenAMEl= document.getElementById("11-a").innerHTML = "<p>" + moment().format("11A") + "</p>";
+var twelvePMEl= document.getElementById("12-p").innerHTML = "<p>" + moment().format("12P") + "M" + "</p>";
+var onePMEl= document.getElementById("1-p").innerHTML = "<p>" + moment().format("1P") + "M" + "</p>";
+var twoPMEl= document.getElementById("2-p").innerHTML = "<p>" + moment().format("2P") + "M" + "</p>";
+var threePMEl= document.getElementById("3-p").innerHTML = "<p>" + moment().format("3P") + "M" + "</p>";
+var fourPMEl= document.getElementById("4-p").innerHTML = "<p>" + moment().format("4P") + "M" + "</p>";
+var fivePMEl= document.getElementById("5-p").innerHTML = "<p>" + moment().format("5P") + "M" + "</p>";
 
 // change background color as time passes
-
-var currentTime = moment().format(hA);
-
-var changeColor = function(timeEl) {
-  // remove time class
-  $(taskEl).removeClass("future");
-  // apply new time class each hour
-  if (moment().isAfter(currentTime)) {
-    $(taskEl).addClass("present");
-  }
-};
+var currentHour = moment().format("hA");
+// if schedule hour === current hour change class to "present"
+var changeColor = function (){
+  
+}
+// if schedule hour < current hour change class to "past"
 
 
-// moment().seconds(30).valueOf() === new Date().setSeconds(30);
-// set hour ---- moment().hour(Number); 
-// get hour ---- moment().hour(); // Number
+// var changeColor = function(timeEl) {
+//   // remove time class
+//   $(taskEl).removeClass("future");
+//   // apply new time class each hour
+//   if (moment().isAfter(currentTime)) {
+//     $(taskEl).addClass("present");
+//   }
+// };
 
 
 // WHEN I scroll down
